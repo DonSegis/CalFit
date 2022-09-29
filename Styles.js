2c3f0b7779ddbc25ed109fa564cb012db4499a47
vndresito <andresmbustosm@gmail.com>
2022-09-29 19:12:22 -0300

    Pasos version 1

---
+++ Styles.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  textDesign: {
+    height: 50,
+    width: "85%",
+    overflow: "hidden",
+    fontSize: 20,
+    paddingLeft: 20,
+    marginLeft: "10%",
+  },
+});
+
+export default styles;
