7a17c51d1627d1cffb5cfb3b815fda6c9d60ed4f
vndresito <andresmbustosm@gmail.com>
2022-09-29 17:33:20 -0300

    navbar

---
+++ screens/Comidas.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+function Comidas() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Comidas!</Text>
+    </View>
+  );
+}
+
+export default Comidas;
