47f2ffefa7ba3dda0d06b4a9c8cfee2045b742b8
seba <sebanorero15@gmail.com>
2022-11-14 23:57:38 -0300

    v4

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 import styles from "../../Styles";
+import { FontAwesome } from "@expo/vector-icons";
+import { database } from "../database/firebase";
+import {
+  deleteDoc,
+  doc,
+  updateDoc,
+  addDoc,
+  collection,
+  query,
+  getDoc,
+} from "firebase/firestore";
 
 export default function Meals({ id, name, totalCalories, date }) {
+  const OnDelete = () => {
+    const docRef = doc(database, "meals", id);
+    deleteDoc(docRef);
+  };
+
   return (
     <View style={styles.boxFood}>
       <View style={styles.boxFoodName}>
@@ -11,6 +27,14 @@ export default function Meals({ id, name, totalCalories, date }) {
       <View style={styles.boxFoodExtar}>
         <Text>Total Calories: {totalCalories}</Text>
         <Text>Date: {date}</Text>
+      </View>
+      <View style={{ marginRight: 10, marginTop: 8 }}>
+        <FontAwesome
+          name="trash-o"
+          size={24}
+          color="black"
+          onPress={() => OnDelete()}
+        />
       </View>
     </View>
   );
