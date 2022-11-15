dca8c7bc046657e79b1b808ef3764d2d72510c77
vndresito <andresmbustosm@gmail.com>
2022-11-14 22:07:59 -0300

    homescreen

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,45 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import CircularProgress from "react-native-circular-progress-indicator";
+import { database } from "../src/database/firebase";
+import { collection, onSnapshot, query, addDoc, doc } from "firebase/firestore";
 
 function HomeScreen() {
+  const [state, setstate] = React.useState([]);
+
+  React.useEffect(() => {
+    const collectionRef = collection(database, "meals");
+    const q = query(collectionRef /*, orderBy("name", "desc")*/);
+
+    const unsuscribe = onSnapshot(q, (querySnapshot) => {
+      setstate(
+        querySnapshot.docs.map((doc) => ({
+          calories: doc.data().totalCalories,
+        }))
+      );
+    });
+    console.log(state);
+    return unsuscribe;
+  }, []);
+  let total = 0;
+  state.map((value) => {
+    total += value.calories;
+    return total;
+  });
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-    </View>
+    <CircularProgress
+      value={total}
+      maxValue={2000}
+      radius={160}
+      textColor="#000000"
+      activeStrokeColor={"#e3291b"}
+      inActiveStrokeColor={"#e8665d"}
+      inActiveStrokeOpacity={0.5}
+      inActiveStrokeWidth={40}
+      activeStrokeWidth={40}
+      title={"/2000"}
+      titleColor={"#000000"}
+      titleStyle={{ fontWeight: "bold" }}
+    />
   );
 }
 
